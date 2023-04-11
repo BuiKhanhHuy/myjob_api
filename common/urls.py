6f939d69adb1809d4a019ed3f5378e055016c54f
huybk2 <huybk2@fpt.com.vn>
2023-04-12 04:23:04 +0700

    cap nhat api cho app

--- common/urls.py
+++ common/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('create-databases/', views.create_database),
     path('configs/', views.get_all_config),
     path('districts/', views.get_districts),
     path('top-careers/', views.get_top_10_careers),
+    path('all-careers/', views.get_all_careers),
     path('demo/', views.demo)
 ]
