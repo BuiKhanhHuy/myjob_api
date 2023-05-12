d54535e286c1badde13e04d16c61423c9257849f
BuiKhanhHuy <khuy220@gmail.com>
2023-05-13 00:10:05 +0700

    Cap STATICFILES_DIRS

--- myjob_api/settings.py
+++ myjob_api/settings.py
@@ -195,6 +195,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
