f2a0e35a19b2b69b1912cab434b6fb1fcf4abf25
BuiKhanhHuy <khuy220@gmail.com>
2023-05-13 12:27:31 +0700

    Cap nhat deploy celery va static file

--- myjob_api/settings.py
+++ myjob_api/settings.py
@@ -201,9 +201,9 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'app/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "myjob", "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, 'app', 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
