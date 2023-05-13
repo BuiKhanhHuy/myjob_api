9247899003ec02830024f74395fe725ffebd7cf6
BuiKhanhHuy <khuy220@gmail.com>
2023-05-13 11:34:03 +0700

    Cap nhat requirements

--- myjob_api/settings.py
+++ myjob_api/settings.py
@@ -111,6 +111,9 @@ WSGI_APPLICATION = 'myjob_api.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+# Bo khi su dung sqlclient: mysqlclient==2.1.1
+import pymysql
+pymysql.install_as_MySQLdb()
 
 DATABASES = {
     'default': {
