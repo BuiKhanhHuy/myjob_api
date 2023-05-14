1c28e3a91c6b2c1616542a1825ff3789a0285cf3
BuiKhanhHuy <khuy220@gmail.com>
2023-05-14 14:45:51 +0700

    Cap nhat deploy

--- myjob_api/settings.py
+++ myjob_api/settings.py
@@ -205,9 +205,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "myjob", "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
