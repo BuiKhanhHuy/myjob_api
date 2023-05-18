20a1e8117cd6ac7afdc0ec421a5210ae7856ceeb
BuiKhanhHuy <khuy220@gmail.com>
2023-05-18 20:28:43 +0700

    Cap nhat STATIC_ROOT

--- myjob_api/settings.py
+++ myjob_api/settings.py
@@ -205,7 +205,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "myjob", "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
